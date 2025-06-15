a3bb291bdc93bfd3df9d6653333d339a616a9254
Swayogi <mrswayogi@gmail.com>
2025-06-15 18:13:39 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,1 +1,9 @@
-adsf
+#!/usr/bin/env python  
+import sys
+for line in sys.stdin:
+    line = line.strip()
+    line = line.split(",")
+    if len(line) >= 2:
+        id = line[0]
+        opinion = line[4]
+        print '%s\t%s' % (id, opinion)  
