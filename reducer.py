e49e6fdc24225d68932ccd57df4dd2b0854ec65f
Swayogi <mrswayogi@gmail.com>
2025-06-15 18:14:04 +0530

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,1 +1,15 @@
-safdl
+#!/usr/bin/env python
+import sys
+opiniondict = {}
+count = 0
+for line in sys.stdin:
+    line = line.strip()
+    pid, opinion = line.split('\t')
+    if opinion in opiniondict:
+        opiniondict[opinion].append(count + 1)
+    else:
+        opiniondict[opinion] = []
+        opiniondict[opinion].append(count + 1)
+for opinion in opiniondict.keys():
+    count = len(opiniondict[opinion])
+    print '%s\t%s' % (opinion, count)
